71f3e08d443ef0574a64c54b62b49abba4cac35c
FayzaanV <fayzaanvirk6@yahoo.com>
2026-04-13 18:34:38 -0500

    Finished JavaScript methods

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+let score = 0;
+
 function randomizeButtonPositions() {
     let buttons = document.querySelectorAll("button");
     buttons.forEach( (b) => {
         b.style.left = (Math.random()*80+10)+"%";
         b.style.top = (Math.random()*80+10)+"%";
     });
+}
+
+function handleDessertClick() {
+    score++;
+    let scoreCount = document.getElementById("score");
+    scoreCount.innerHTML ="<p id=\"score\">Score: " + score + "</p>";
+    randomizeButtonPositions();
+}
+
+function handleDesertClick() {
+    score--;
+    randomizeButtonPositions();
+    let scoreCount = document.getElementById("score");
+    scoreCount.innerHTML = "<p id=\"score\">Score: " + score + "</p>";
 }
