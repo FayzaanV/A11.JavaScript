059dc1529e6081527b32355f46997aff775a9a04
FayzaanV <fayzaanvirk6@yahoo.com>
2026-04-13 18:18:28 -0500

    Implemented randomizeButtonPositions() method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function randomizeButtonPositions() {
-    let buttons = document.querySelectorAll("FIXME");
+    let buttons = document.querySelectorAll("button");
     buttons.forEach( (b) => {
         b.style.left = (Math.random()*80+10)+"%";
         b.style.top = (Math.random()*80+10)+"%";
